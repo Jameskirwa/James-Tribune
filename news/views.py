4347547efd5742afeba1bd3cd99d3289030e9844
James Kirwa <example@email.com>
2019-09-22 20:28:10 +0300

    import redirect in views

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 
@@ -47,3 +47,20 @@ def welcome(request):
 def news_of_day(request):
     date = dt.date.today()
     return render(request, 'all-news/today-news.html', {"date": date,})
+
+# View Function to present news from past days
+def past_days_news(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
