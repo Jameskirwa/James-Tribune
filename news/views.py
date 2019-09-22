e19a75ec714d42cfefb62e410a56bb85eb151cac
James Kirwa <example@email.com>
2019-09-22 15:19:45 +0300

    converts date objects to find exact date

--- news/views.py
+++ news/views.py
@@ -8,3 +8,5 @@ view takes request from a user prosses it and it returns response to the user
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the James Tribune')
+def news_of_day(request):
+    date = dt.date.today()
