a2696a494f505a040af07d31b1843f60dd6a799d
James Kirwa <example@email.com>
2019-09-22 14:52:14 +0300

    def a fuction in view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# """
+# view takes request from a user prosses it and it returns response to the user
+# """
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the James Tribune')
