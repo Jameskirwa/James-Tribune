08d2fb690fa520b9c1d76fbf5a35d238c9ed2f7a
James Kirwa <example@email.com>
2019-09-22 14:52:48 +0300

    add comment to view file

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-# """
-# view takes request from a user prosses it and it returns response to the user
-# """
+"""
+view takes request from a user prosses it and it returns response to the user
+"""
 
 # Create your views here.
 def welcome(request):
